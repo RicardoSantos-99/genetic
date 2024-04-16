d0e796bcfbf1c7988734a60cd4b3a4fc182d052b
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2024-04-15 21:26:58 -0300

    impl cargo solution with genotype

--- lib/genetic.ex
+++ lib/genetic.ex
@@ -4,23 +4,27 @@ defmodule Genetic do
   def run(problem, opts \\ []) do
     population = initialize(&problem.genotype/0)
 
-    evolve(population, problem, opts)
+    first_generation = 0
+
+    evolve(population, problem, first_generation, opts)
   end
 
-  def evolve(population, problem, opts \\ []) do
+  def evolve(population, problem, generation, opts \\ []) do
     population = evaluate(population, &problem.fitness_function/1, opts)
     best = hd(population)
 
     IO.write("\r Current Best #{best.fitness}")
 
-    if problem.terminate?(population) do
+    if problem.terminate?(population, generation) do
       best
     else
+      generation = generation + 1
+
       population
       |> select(opts)
       |> crossover(opts)
       |> mutation(opts)
-      |> evolve(problem, opts)
+      |> evolve(problem, generation, opts)
     end
   end
 
--- scripts/one_max.exs
+++ scripts/one_max.exs
@@ -13,7 +13,27 @@ defmodule OneMax do
   def fitness_function(chromosome), do: Enum.sum(chromosome.genes)
 
   @impl true
-  def terminate?([best | _]), do: best.fitness == 42
+  def terminate?(_population, generation), do: generation == 100
+
+  # @impl true
+  # def terminate?(population, _generation) do
+  #   Enum.max_by(population, &OneMax.fitness_function/1).fitness == 42
+  # end
+
+  # @impl true
+  # def terminate?(population, _generation) do
+  #   Enum.min_by(population, &OneMax.fitness_function/1).fitness == 0
+  # end
+
+  # @impl true
+  # def terminate?(population, _generation) do
+  #   avg = Enum.map(population, &(Enum.sum(&1) / length(&1)))
+
+  #   avg == 21
+  # end
+
+  # @impl true
+  # def terminate?([best | _], _generation), do: best.fitness == 42
 end
 
 soln = Genetic.run(OneMax)
